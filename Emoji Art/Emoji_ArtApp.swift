e415cfc55a3ebaec72729c9ad7aa26752ca5a69b
EaRu723 <y.andrearusso@gmail.com>
2023-12-13 17:11:32 -0500

    Initial Commit

---
+++ Emoji Art/Emoji_ArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_ArtApp.swift
+//  Emoji Art
+//
+//  Created by Andrea Russo on 12/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_ArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

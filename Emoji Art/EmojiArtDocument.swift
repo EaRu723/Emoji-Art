1d24047fbc2570e6881e257da99e3871102357f5
EaRu723 <y.andrearusso@gmail.com>
2023-12-14 12:40:10 -0500

    Debugged

--- Emoji Art/EmojiArtDocument.swift
+++ Emoji Art/EmojiArtDocument.swift
@@ -5,4 +5,45 @@
 //  Created by Andrea Russo on 12/13/23.
 //
 
-import Foundation
+import SwiftUI
+
+class EmojiArtDocument: ObservableObject {
+    typealias Emoji = EmojiArt.Emoji
+    @Published private var emojiArt = EmojiArt()
+    
+    init() {
+        emojiArt.addEmoji("🌚", at: .init(x: -250, y: -50), size:50)
+        emojiArt.addEmoji("🌝", at: .init(x: 250, y: -50), size:50)
+    }
+    
+    var emojis: [Emoji] {
+        emojiArt.emojis
+    }
+    
+    var background: URL? {
+        emojiArt.background
+    }
+    
+    // MARK: - Intent(s)
+    
+    func setBackground(_ url: URL?) {
+        emojiArt.background = url
+    }
+    
+    func addEmoji(_ emoji: String, at position: Emoji.Position, size: CGFloat) {
+        emojiArt.addEmoji(emoji, at: position, size: Int(size))
+    }
+}
+
+extension EmojiArt.Emoji {
+    var font: Font {
+        Font.system(size: CGFloat(size))
+    }
+}
+
+extension EmojiArt.Emoji.Position {
+    func `in`(_ geometry: GeometryProxy) -> CGPoint {
+        let center = geometry.frame(in: .local).center
+        return CGPoint(x: center.x + CGFloat(x), y: center.y - CGFloat(y))
+    }
+}
